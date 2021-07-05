c75882b81fbead1162890b159e68afcfd90701a5
Paulo César Lins <paulocsar91@gmail.com>
2021-07-05 01:26:26 -0300

    Cria projeto vuejs

---
+++ frontend/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul>
+    <li v-for="user of users" v-bind:key="user">
+      {{user.name}} - {{user.email}}
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from 'axios'
+
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      users: []
+
+    }
+  },
+  created: function(){
+    axios.get('http://localhost:8000/api/users')
+      .then(result => {
+        this.users = result.data;
+      }).catch(error => {
+        console.log(error);
+      })
+  }
+}
+</script>
+
+<style>
+
+</style>
