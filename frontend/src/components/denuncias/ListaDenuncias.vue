8d7a1dc89468e0100c1789e78c606e36159415cc
Paulo César Lins <paulocsar91@gmail.com>
2021-07-19 10:53:37 -0300

    Implementa a função remover denúncia

--- frontend/src/components/denuncias/ListaDenuncias.vue
+++ frontend/src/components/denuncias/ListaDenuncias.vue
@@ -7,6 +7,8 @@
         <div class="col s12">
           <a href="#!" class="breadcrumb">Início</a>
           <a href="#!" class="breadcrumb">Denúncias</a>
+          <a href="#!" class="breadcrumb">Cadastro de denúncias</a>
+          <a href="#!" class="breadcrumb">Lista de denúncias</a>
         </div>
       </div>
     </nav>
@@ -23,18 +25,24 @@
           <div class="card company-card animate__animated animate__fadeInUp animate__delay-1s" v-for="denuncia of denuncias" v-bind:key="denuncia">
             <h5><i class="fas fa-map-marker-alt"></i>
             
-            Endereço: {{denuncia.endereco}}
-            <p>Número: {{denuncia.numero}}</p>
-            <p>Bairro: {{denuncia.bairro}}</p>
-            <p>Cidade: {{denuncia.cidade}}</p>
+            <a v-if="!denuncia.editar">Endereço: {{denuncia.endereco}} <input v-if="denuncia.editar" type="text" v-model="denuncia.endereco"> </a>
+            <p v-if="!denuncia.editar">Número: {{denuncia.numero}} <input v-if="denuncia.editar" type="text" v-model="denuncia.numero"></p> 
+            <p v-if="!denuncia.editar">Bairro: {{denuncia.bairro}} <input v-if="denuncia.editar" type="text" v-model="denuncia.bairro"></p>
+            <p v-if="!denuncia.editar">Cidade: {{denuncia.cidade}} <input v-if="denuncia.editar" type="text" v-model="denuncia.cidade"></p>
             
             </h5>
-            <p>Denúncia: {{denuncia.descricao}}</p>
+            
+            <p v-if="!denuncia.editar">Denúncia: {{denuncia.descricao}} <input v-if="denuncia.editar" type="text" v-model="denuncia.descricao"></p>
             
 
             <div class="card-actions animate__animated animate__fadeIn animate__delay-2s">
-              <span><i class="far fa-edit"></i></span>
-              <span><i class="far fa-trash-alt"></i></span>
+              
+              <span v-if="!denuncia.editar" v-on:click="editInfo(denuncia.id)"><i class="far fa-edit"></i></span>
+              <span v-if="!denuncia.editar" v-on:click="remover(denuncia.id)"><i class="far fa-trash-alt"></i></span>
+
+              <span v-if="denuncia.editar" class="confirm-button"><i class="far fa-check-circle"></i></span>
+              <span v-if="denuncia.editar" class="remove-button"><i class="far fa-times-circle"></i></span>
+            
             </div>
           </div>
         </div>
@@ -52,16 +60,27 @@ export default {
   data(){
     return {
       denuncias: [
-        { endereco: 'Rua Primeiro de Maio', numero: '395', bairro: 'Santa Luzia', cidade: 'Palmares/PE', descricao:'Lixo na calçada' },
-        { endereco: 'Rua Ivanildo Lins', numero: '1257', bairro: 'Santa Rosa', cidade: 'Palmares/PE', descricao:'Entulho de construção no meio da rua' },
-        { endereco: 'Praça Santo Amaro', numero: '', bairro: 'Santo Amaro', cidade: 'Palmares/PE', descricao:'Lixo na praça' },
-        { endereco: 'Travessa Tenente Everaldo', numero: '1365', bairro: 'Santo Onofre', cidade: 'Palmares/PE', descricao:'O lixo não é coletado há 12 dias' },
-        { endereco: 'Travessa Luiza Pedroza', numero: '125', bairro: 'Coabe 2', cidade: 'Palmares/PE', descricao:'O carro da coleta não passa há 8 dias' },
-        { endereco: 'Praça Paulo Paranhos', numero: '', bairro: 'Centro', cidade: 'Palmares/PE', descricao:'O lixo das lixeiras não está sendo recolhido' },
+        { id: 1 , endereco: 'Rua Primeiro de Maio', numero: '395', bairro: 'Santa Luzia', cidade: 'Palmares/PE', descricao:'Lixo na calçada' },
+        { id: 2 , endereco: 'Rua Ivanildo Lins', numero: '1257', bairro: 'Santa Rosa', cidade: 'Palmares/PE', descricao:'Entulho de construção no meio da rua' },
+        { id: 3 , endereco: 'Praça Santo Amaro', numero: '', bairro: 'Santo Amaro', cidade: 'Palmares/PE', descricao:'Lixo na praça' },
+        { id: 4 , endereco: 'Travessa Tenente Everaldo', numero: '1365', bairro: 'Santo Onofre', cidade: 'Palmares/PE', descricao:'O lixo não é coletado há 12 dias' },
+        { id: 5 , endereco: 'Travessa Luiza Pedroza', numero: '125', bairro: 'Coabe 2', cidade: 'Palmares/PE', descricao:'O carro da coleta não passa há 8 dias' },
+        { id: 6 , endereco: 'Praça Paulo Paranhos', numero: '', bairro: 'Centro', cidade: 'Palmares/PE', descricao:'O lixo das lixeiras não está sendo recolhido' },
          
       ]
     }
   },
+
+  methods: {
+    remover(id) {
+      this.denuncias = this.denuncias.filter((denuncia) => denuncia.id != id);
+    },
+
+    editInfo(){
+      
+    }
+  },
+
   created: function() {
     
   }
@@ -138,5 +157,12 @@ h2 span {
 
 p {
     margin-left: 24px;
+}
+
+.fa-check-circle{
+  color: green;
+}
+.fa-times-circle{
+  color: red;
 }
 </style>
